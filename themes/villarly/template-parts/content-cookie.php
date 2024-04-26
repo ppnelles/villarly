eb445ff75045dea4cd2f0c6268c27974c2b7291b
Paul-Philippe Nelles <ppnelles@gmail.com>
2024-04-26 16:04:55 +0200

    cookies

--- themes/villarly/template-parts/content-cookie.php
+++ themes/villarly/template-parts/content-cookie.php
@@ -1,4 +1,4 @@
 <div id="cookiebanner" class="hidden">
-	<div class="intro"><?php _e('This website uses cookies.', '_turbo') ?></div> 
-	<div><button class="cookie-btn"><?php _e('Fine by me','_turbo') ?></button> <a href="<?php echo get_privacy_policy_url(); ?>"><?php 	_e("More info", '_turbo') ?></a></div>
+	<div class="intro"><?php _e('Ce site utilise des cookies.', '_turbo') ?></div> 
+	<div><button class="cookie-btn"><?php _e('OK','_turbo') ?></button> <a href="<?php echo get_privacy_policy_url(); ?>"><?php 	_e("Plus d'infos", '_turbo') ?></a></div>
 </div>
