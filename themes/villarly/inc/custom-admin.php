572b0b0551450dd88a6658bc3dae2283a1b99c27
Paul-Philippe Nelles <ppnelles@gmail.com>
2024-05-07 10:33:27 +0200

    prepa admin + socials

--- themes/villarly/inc/custom-admin.php
+++ themes/villarly/inc/custom-admin.php
@@ -20,3 +20,24 @@ function custom_login_logo_url() {
     return home_url();
 }
 add_filter( 'login_headerurl', 'custom_login_logo_url' );
+
+
+if(current_user_can('editor')) {
+add_action('admin_head', 'my_custom_fonts');
+
+function my_custom_fonts() {
+  echo '<style>
+    #menu-posts,
+    #menu-users,
+    #menu-tools,
+    #menu-posts-calendar,
+    #toplevel_page_edit-post_type-acf-field-group,
+    #toplevel_page_members,
+    #toplevel_page_smush,
+    #toplevel_page_theme-general-settings,
+    #menu-settings {
+      display: none;
+    } 
+  </style>';
+}
+}
